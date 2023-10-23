f9b66a4786a31f5a144c5dd87f27f227471a4b37
Angus <superangus@protonmail.com>
2023-10-23 16:15:25 +0200

    fix gitignore

--- src/main/scala/ginseng/Disciple.scala
+++ src/main/scala/ginseng/Disciple.scala
@@ -1,11 +1,11 @@
 package ginseng
 
 /**
- * Represents an entity with a unique identifier and generation number.
+ * Represents a Disciple with a unique identifier and generation number.
  *
- * @param id  The unique identifier of the entity.
- * @param gen The generation of the Entity, used to track the lifetime of the Entity
- *            When an Entity is deallocated, its generation is incremented, which invalidates any references to the Entity.
- *            When an Entity is allocated, its generation is set to 0, which means that the Entity is alive and can be used.
+ * @param id  The unique identifier of the Disciple.
+ * @param gen The generation of the Disciple, used to track the lifetime of the Disciple
+ *            When an Disciple is deallocated, its generation is incremented, which invalidates any references to the Disciple.
+ *            When an Disciple is allocated, its generation is set to 0, which means that the Disciple is alive and can be used.
  */
 case class Disciple(id: Int, gen: Int)
